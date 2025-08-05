2e92b7e47d70036fdfc90d942d7407b99438c1a6
Anıl Beyazoglu <anil@beyazoglu.com>
2025-08-05 18:54:44 +0300

    CSP headers added

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,8 @@ export default defineConfig({
   },
   server: {
     allowedHosts: ['.e2b.dev', '.docy.app', '.docy.dev'],
+    headers: {
+      'Content-Security-Policy': "frame-ancestors 'self' http://localhost:3000 https://localhost:3000 https://alpha.build.docyrus.app https://build.docyrus.app https://docyrus.app https://alpha.build.docy.app https://docy.app",
+    },
   },
 })
